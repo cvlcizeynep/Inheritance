999f97b8ab0b71f82437c58a6ff5e7e9e204ca61
Zeynep Cuvalci <117526085+cvlcizeynep@users.noreply.github.com>
2023-08-28 13:58:53 +0300

    Add files via upload

---
+++ Hayvanlar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.InheritanceOdev
+{
+   public abstract class Hayvanlar
+    {
+       public string _name;
+       public string _tur;
+       public   int _yas;
+        public Hayvanlar() { }
+        public Hayvanlar(string name,string tur,int yas)
+        {
+            _name = name;
+            _tur = tur;
+            _yas = yas;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Hayvanin turu {_tur},hayvanin ismi {_name},hayvanin yasi {_yas}";
+        }
+
+        public abstract void sesCikar();
+        
+    }
+    public class Kedi:Hayvanlar
+    {
+        public override void sesCikar()
+        {
+            Console.WriteLine("Kediler miyavv der");
+        } 
+    }
+    public class Kopek : Hayvanlar     
+    {
+        public override void sesCikar()
+        {
+            Console.WriteLine("Kopekler havlar");
+        }
+
+    }
+public class Kus : Hayvanlar
+    {
+        public override void sesCikar()
+        {
+            Console.WriteLine("Kuslar oter");
+        }
+    }
+
+   
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp1.InheritanceOdev;
+
+Hayvanlar hayvanlar = new Kedi();
+hayvanlar._tur = "Kedi";
+hayvanlar._name = "Moka";
+hayvanlar._yas = 2;
+Console.WriteLine(hayvanlar);
+hayvanlar.sesCikar();
+Kedi kedi = new Kedi();
+kedi.sesCikar();
+Kopek kopus = new Kopek();
+kopus.sesCikar();
+
+kedi._tur = "Scottish";
+kedi._yas = 5;
+kedi._name = "Cakil";
+Console.WriteLine(kedi);
+Hayvanlar Kus = new Kus();
+Kus._tur = "Saka";
+Kus._name = "Mavis";
+Kus._yas = 3;
+Kus.sesCikar();
+Console.WriteLine(Kus);
+
+
+
+
+
+
+
+
+
